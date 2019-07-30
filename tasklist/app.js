9c024f7ac4bdb755916afe83034318a42c036292
Irinnahar <irincse.bu@gmail.com>
2019-07-30 15:21:33 +0600

    remove item from list

--- tasklist/app.js
+++ tasklist/app.js
@@ -1,9 +1,14 @@
 const form = document.querySelector('#task-form');
 const inputText = document.querySelector('#task');
 const taskList = document.querySelector('.collection');
+const clearTask = document.querySelector('.clear-tasks');
 
 form.addEventListener('submit', addTask);
+taskList.addEventListener('click', removeTask);
+clearTask.addEventListener('click', clearAllTask);
 
+
+// add a task
 function addTask (e) {
   e.preventDefault();
   
@@ -11,6 +16,29 @@ function addTask (e) {
   task.className = 'collection-item';
   task.textContent = inputText.value;
 
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  task.appendChild(link);
+
+  // console.log(task)
   taskList.appendChild(task);
   inputText.value = '';
+}
+
+// remove a task
+function removeTask(e){
+  if(e.target.parentElement.classList.contains('delete-item')){
+    if(confirm('Are you sure?')){
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// clear all task
+function clearAllTask(e){
+  // taskList.innerHTML = '';
+  while(taskList.firstChild){
+    taskList.firstChild.remove();
+  }
 }
